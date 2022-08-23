abcbfd24e6a4fbf3f6ee489b49682ed0846d96a9
vsenku <vsenku@gmail.com>
2022-08-23 07:20:01 +0530

    Initial Commit

---
+++ src/main/java/com/example/jenkins/JentestCitestApplication.java
@@ -0,0 +1,27 @@
+package com.example.jenkins;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JentestCitestApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JentestCitestApplication.class);
+	
+	@PostConstruct
+	public void init(){
+		logger.info("Application Initiated......");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		SpringApplication.run(JentestCitestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins/JentestCitestApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JentestCitestApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(JentestCitestApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case started...");
+		assertEquals(true, true);
+	}
+
+}
